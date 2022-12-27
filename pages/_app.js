27e9bd94197e65c8997ecb0135616ba39ca92d32
TrisnaWahyudi <wayantrisna79@gmail.com>
2022-12-27 21:25:08 +0800

    add favicon to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -10,6 +10,12 @@ export default function App({ Component, pageProps }) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
+        <link
+          rel="icon"
+          type="image/x-icon"
+          href="/img/brand/favicon.png"
+        ></link>
+
         <title>Airtrip</title>
       </Head>
       <Component {...pageProps} />
