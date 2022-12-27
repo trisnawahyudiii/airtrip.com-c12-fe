af62cd4bdf82480de6198ac4214fd3419848f35a
TrisnaWahyudi <wayantrisna79@gmail.com>
2022-12-27 12:56:16 +0800

    move document head to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,18 @@
 import "../styles/globals.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <title>Airtrip</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
