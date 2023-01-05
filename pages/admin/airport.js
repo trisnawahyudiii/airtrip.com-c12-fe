d3265345b0a2bcabffd2b76c62a1acc277c437ca
NaufalAmr <amiruddin0895@gmail.com>
2023-01-05 15:17:00 +0700

    fix airport header error

--- pages/admin/airport.js
+++ pages/admin/airport.js
@@ -2,9 +2,9 @@ import Layout from "../../layouts/admin/Layout";
 import Airport from "../../components/admin/airport/Airport";
 import { getServerSideProps } from "./index";
 
-export default function airportPage() {
+export default function airportPage({ user }) {
   return (
-    <Layout title="Airport">
+    <Layout title="Airport" user={user}>
       <Airport />
     </Layout>
   );
